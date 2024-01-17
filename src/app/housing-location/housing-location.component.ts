6efd0e3e30b79acc2baafaf2bbb91749e4ef13b6
Navindu Dahanayake <NavinduD@99x.io>
2024-01-17 16:48:29 +0530

    Create housing-location component

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent {
+  @Input() housingLocation! : HousingLocation;
+}
